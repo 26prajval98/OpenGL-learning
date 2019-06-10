eaf7f9843d71c4342293e298d3a7230f3ccf57c3
Prajval M <26prajval98@gmail.com.com>
2019-06-10 12:53:53 +0530

    Index draw and error handling

--- ogl/main.cpp
+++ ogl/main.cpp
@@ -4,6 +4,52 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+#include <assert.h>
+
+#define ASSERT(x) if (!(x)) __debugbreak();
+#define GLErrorWrapper(x) GLClearError(); x; ASSERT(GLCheckError());
+
+static void GLClearError()
+{
+	while (glGetError() != GL_NO_ERROR);
+}
+
+static bool GLCheckError()
+{
+	while (GLenum error = glGetError())
+	{
+
+		std::cout << "[OpenGL Error] ";
+		switch (error) {
+		case GL_INVALID_ENUM:
+			std::cout << "GL_INVALID_ENUM : An unacceptable value is specified for an enumerated argument.";
+			break;
+		case GL_INVALID_VALUE:
+			std::cout << "GL_INVALID_OPERATION : A numeric argument is out of range.";
+			break;
+		case GL_INVALID_OPERATION:
+			std::cout << "GL_INVALID_OPERATION : The specified operation is not allowed in the current state.";
+			break;
+		case GL_INVALID_FRAMEBUFFER_OPERATION:
+			std::cout << "GL_INVALID_FRAMEBUFFER_OPERATION : The framebuffer object is not complete.";
+			break;
+		case GL_OUT_OF_MEMORY:
+			std::cout << "GL_OUT_OF_MEMORY : There is not enough memory left to execute the command.";
+			break;
+		case GL_STACK_UNDERFLOW:
+			std::cout << "GL_STACK_UNDERFLOW : An attempt has been made to perform an operation that would cause an internal stack to underflow.";
+			break;
+		case GL_STACK_OVERFLOW:
+			std::cout << "GL_STACK_OVERFLOW : An attempt has been made to perform an operation that would cause an internal stack to overflow.";
+			break;
+		default:
+			std::cout << "Unrecognized error" << error;
+		}
+		std::cout << std::endl;
+		return false;
+	}
+	return true;
+}
 
 struct ShaderProgramSource
 {
@@ -115,16 +161,29 @@ int main(void)
 	}
 
 	float pos[] = {
-		0.0f, 1.0f,
-		-1.0f, -1.0f,
-		+1.0f, -1.0f,
+		-0.5f, -0.5f,
+		0.5f, -0.5f,
+		0.5f, 0.5f,
+		-0.5f, 0.5f,
+	};
+
+	unsigned int indices[] = {
+		0, 1, 2,
+		2, 3, 0,
 	};
 
 	GLuint buffer;
 	glCreateBuffers(1, &buffer);
 	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), pos, GL_STATIC_DRAW);
-	
+	glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), pos, GL_STATIC_DRAW);
+
+
+	//Either bind here or bind in glDrawElements but not both
+	//GLuint index_buffer;							
+	//glCreateBuffers(1, &index_buffer);
+	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, index_buffer);
+	//glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(unsigned int), indices, GL_STATIC_DRAW);
+
 	ShaderProgramSource source = ParseShader("lol.shader");
 
 	unsigned int shader = CreateShader(source.VertexSource, source.FragmentSource);
@@ -139,8 +198,7 @@ int main(void)
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
-
+		GLErrorWrapper(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, indices));
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
 
